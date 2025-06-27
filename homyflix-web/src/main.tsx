bf2bc8cb52c700a0a1e4ecddbaccf07112e28b05
uesleinasch <uesleinac@gmail.com>
2025-06-26 21:42:45 -0300

    feat(19): Configurar Redux store

--- homyflix-web/src/main.tsx
+++ homyflix-web/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import App from './App.tsx'
 import './index.css'
 import '@fontsource/roboto/300.css';
@@ -10,8 +12,10 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ homyflix-web/src/store/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// Importe os reducers dos slices aqui quando eles forem criados
+// import movieReducer from './slices/movieSlice';
+// import userReducer from './slices/userSlice';
+
+export const store = configureStore({
+  reducer: {
+    // Adicione os reducers aqui
+    // movies: movieReducer,
+    // user: userReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: import.meta.env.MODE !== 'production',
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
