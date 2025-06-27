768998c84ba89452c78136a7c569659898a79da5
uesleinasch <uesleinac@gmail.com>
2025-06-27 07:57:08 -0300

    feat(20):Criar MovieService

--- homyflix-web/src/types/movie.ts
+++ homyflix-web/src/types/movie.ts
@@ -3,8 +3,15 @@ export interface Movie {
   title: string;
   release_year: number;
   genre: string;
+  director: string;
   synopsis: string;
+  user_id: number;
+  created_at: string;
+  updated_at: string;
 }
+
+export type MovieCreateData = Omit<Movie, 'id' | 'user_id' | 'created_at' | 'updated_at'>;
+export type MovieUpdateData = Partial<MovieCreateData>;
 
 export interface MovieState {
   movies: Movie[];
