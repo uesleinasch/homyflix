3b674482d5adef8bd2aaf869b88544b77e5aa7c1
uesleinasch <uesleinac@gmail.com>
2025-06-27 20:53:13 -0300

    style(21): page direction

--- homyflix-web/src/App.tsx
+++ homyflix-web/src/App.tsx
@@ -1,44 +1,17 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import DashboardPage from './pages/DashboardPage';
-import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/login-page/LoginPage';
+import RegisterPage from './pages/register-page/RegisterPage';
+import DashboardPage from './pages/dashboard-page/DashboardPage';
+import NotFoundPage from './pages/notfound-page/NotFoundPage';
 import PrivateRoute from './components/auth/PrivateRoute';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <MantineProvider>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <h2>API Connection Test:</h2>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Routes>
         {/* Public Routes */}
         <Route path="/login" element={<LoginPage />} />
