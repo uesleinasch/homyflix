e439621f913c44bba809676da26b317e19e4163e
uesleinasch <uesleinac@gmail.com>
2025-06-27 20:43:38 -0300

    feat(21): Implementar roteamento

--- homyflix-web/src/App.tsx
+++ homyflix-web/src/App.tsx
@@ -1,5 +1,11 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import NotFoundPage from './pages/NotFoundPage';
+import PrivateRoute from './components/auth/PrivateRoute';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,6 +39,19 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Routes */}
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+
+        {/* Not Found Route */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </MantineProvider>
   )
 }
---
+++ homyflix-web/src/components/auth/PrivateRoute.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import type { RootState } from '../../store/store';
+
+const PrivateRoute: React.FC = () => {
+  const { token } = useSelector((state: RootState) => state.auth);
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
