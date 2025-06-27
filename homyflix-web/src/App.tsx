8f9db930a96aedfb6925dda09772ff6984add108
uesleinasch <uesleinac@gmail.com>
2025-06-26 21:39:13 -0300

    fix(19): remove fetch

--- homyflix-web/src/App.tsx
+++ homyflix-web/src/App.tsx
@@ -1,23 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [apiResponse, setApiResponse] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:8000/api/user') // A porta 8000 é a porta exposta do backend no docker-compose
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => setApiResponse(JSON.stringify(data, null, 2)))
-      .catch(error => setApiResponse(`Error fetching data: ${error.message}`));
-  }, []);
 
   return (
     <>
@@ -40,7 +27,6 @@ function App() {
       </div>
       <div className="card">
         <h2>API Connection Test:</h2>
-        <pre>{apiResponse}</pre>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
