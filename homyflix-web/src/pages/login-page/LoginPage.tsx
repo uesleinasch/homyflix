024d4361cf1f4be22883a2060508d2b6049a72b3
uesleinasch <uesleinac@gmail.com>
2025-06-27 21:03:13 -0300

    feat(21): place pages

--- homyflix-web/src/pages/login-page/LoginPage.tsx
+++ homyflix-web/src/pages/login-page/LoginPage.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+       
+      </form>
+      <Link to="/register">
+       <button>Register</button>
+      </Link>
+    </div>
+  );
+
 };
 
 export default LoginPage;
--- homyflix-web/src/pages/register-page/RegisterPage.tsx
+++ homyflix-web/src/pages/register-page/RegisterPage.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <Link to="/login">
+        <button>Ir para login</button>
+      </Link>
+    </div>
+  )
 };
 
 export default RegisterPage;
